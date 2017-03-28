d0b45fa712fd0b99bd8bc53894f7d59570402a8d
Paul Landes <landes@mailc.net>
2017-03-28 08:44:54 -0500

    cleanup

--- src/clojure/zensols/erg/core.clj
+++ src/clojure/zensols/erg/core.clj
@@ -18,6 +18,3 @@
   (cli/set-program-name "pp2erg")
   (-> (create-action-context)
       (cli/process-arguments args)))
-
-(binding [cli/*dump-jvm-on-error* false]
-  (-main "-f" "244" "-e" "-p" "/d/3x10 power hill intervals.ppsmrx" ))
